dde1bc82b5ff322d5d25ba425589a49d4566c045
myimac <noiagendl@hotmail.com>
2020-12-07 15:21:28 +0200

    initialization node.

---
+++ src/api/nodeG.java
@@ -0,0 +1,76 @@
+package api;
+
+import java.util.Objects;
+
+public class nodeG implements node_data {
+    int key, tag;
+    String info;
+    double weighted;
+
+    public nodeG(int key) {
+        this.key = key;
+    }
+
+    @Override
+    public int getKey() {
+        return key;
+    }
+
+    @Override
+    public geo_location getLocation() {
+        return null;
+    }
+
+    @Override
+    public void setLocation(geo_location p) {
+
+    }
+
+    @Override
+    public double getWeight() {
+        return weighted;
+    }
+
+    @Override
+    public void setWeight(double w) {
+        weighted=w;
+    }
+
+    @Override
+    public String getInfo() {
+        return info;
+    }
+
+    @Override
+    public void setInfo(String s) {
+
+    }
+
+    @Override
+    public int getTag() {
+        return tag;
+    }
+
+    @Override
+    public void setTag(int t) {
+        tag=t;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        nodeG nodeG = (nodeG) o;
+        return key == nodeG.key &&
+                tag == nodeG.tag &&
+                Double.compare(nodeG.weighted, weighted) == 0 &&
+                info.equals(nodeG.info);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, tag, info, weighted);
+    }
+
+
+}
