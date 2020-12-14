f8b13422a244fc81ec4f322537c5bf63217ae0db
myimac <noiagendl@hotmail.com>
2020-12-14 21:54:51 +0200

    update

--- src/gameClient/Ex2.java
+++ src/gameClient/Ex2.java
@@ -13,7 +13,6 @@ import java.util.*;
 public class Ex2 implements Runnable {
     private static MyFrame _win;
     private static Arena _ar;
-    private static ArrayList<Object> ans;
 
     public static void main(String[] a) {
         Thread client = new Thread(new Ex2_Client());
@@ -22,7 +21,7 @@ public class Ex2 implements Runnable {
 
     @Override
     public void run() {
-        int scenario_num = 11;
+        int scenario_num = 1;
         game_service game = Game_Server_Ex2.getServer(scenario_num); // you have [0,23] games
         int id = 312321722;
         game.login(id);
@@ -38,7 +37,7 @@ public class Ex2 implements Runnable {
         long dt = 100;
 
         while (game.isRunning()) {
-            moveAgants(game, gg);
+            moveAgants(game, _ar.getGraph() );
             try {
                 if (ind % 1 == 0) {
                     _win.repaint();
@@ -71,18 +70,27 @@ public class Ex2 implements Runnable {
         String fs = game.getPokemons();
         List<CL_Pokemon> ffs = Arena.json2Pokemons(fs);
         _ar.setPokemons(ffs);
-        for (int i = 0; i < log.size(); i++) {
-            CL_Agent ag = log.get(i);
-            int id = ag.getID();
-            int dest = ag.getNextNode();
-            int src = ag.getSrcNode();
-            double v = ag.getValue();
-            if (dest == -1) {
-                dest = nextNode(gg, src);
-                game.chooseNextEdge(ag.getID(), dest);
-                System.out.println("Agent: " + id + ", val: " + v + "   turned to node: " + dest);
-            }
-        }
+//        for (int i = 0; i < log.size(); i++) {
+//            CL_Agent ag = log.get(i);
+//            int id = ag.getID();
+//            int dest = ag.getNextNode();
+//            int src = ag.getSrcNode();
+//            double v = ag.getValue();
+//            if (dest == -1) {
+//                dest = nextNode(gg, src);
+//                game.chooseNextEdge(ag.getID(), dest);
+//                System.out.println("Agent: " + id + ", val: " + v + "   turned to node: " + dest);
+//            }
+//        }
+        ArrayList<Object> a=chooseAgentToMove(log,ffs);
+        CL_Agent ag=(CL_Agent)a.get(0);
+        CL_Pokemon poc=(CL_Pokemon) a.get(1);
+        ArrayList<node_data> path=(ArrayList<node_data>)a.get(2);
+       for(int i=0; i<path.size(); i++){
+           game.chooseNextEdge(ag.getID(),path.get(i).getKey());
+           System.out.println("Agent: " + ag.getID() + ", val: " + ag.getValue() + "   turned to node: " + path.get(i).getKey());
+       }
+
     }
 
     /**
@@ -167,15 +175,68 @@ public class Ex2 implements Runnable {
     }
 
 
-    private static void chasAgentToMove(List<CL_Agent> agents, List<CL_Pokemon> pokemons) {
-        ans.clear();
+    private static ArrayList<Object> chooseAgentToMove(List<CL_Agent> agents, List<CL_Pokemon> pokemons) {
+        ArrayList<Object> ans=new ArrayList<>();
         CL_Pokemon destPoc = pokemons.get(0);
         CL_Agent srcAgent = agents.get(0);
         directed_weighted_graph gameGraphPointer = _ar.getGraph();
         dw_graph_algorithms gAlgo = new DWGraph_Algo();
         gAlgo.init(_ar.getGraph());
-        List<node_data> sp = gAlgo.shortestPath(srcAgent.getSrcNode(), destPoc.get_edge().getDest());
-        
+        ArrayList<node_data> SP=giveShorterPath(srcAgent,destPoc);
+
+        for(int i=0; i<agents.size(); i++){
+            CL_Agent agentPointer=agents.get(i);
+            for(int j=0; j<pokemons.size(); j++){
+                CL_Pokemon pokemonPointer=pokemons.get(j);
+                if(SP==null){
+                    SP=giveShorterPath(srcAgent,destPoc);
+                }
+                else{
+                    ArrayList<node_data> SP2=giveShorterPath(agentPointer,pokemonPointer);
+                    if(SP2!=null){
+                        if(SP.size()>SP2.size()){
+                            ans.clear();
+                          SP=giveShorterPath(agentPointer,pokemonPointer);
+                          ans.add(agentPointer);
+                          ans.add(pokemonPointer);
+                          ans.add(SP);
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
     }
+
+
+    /**
+     * return shorter path.
+     * return null if the is no path.
+     * return null if the shorter Path didnt work
+     * @param agent
+     * @param pokemon
+     * @return
+     */
+    public static ArrayList<node_data> giveShorterPath(CL_Agent agent, CL_Pokemon pokemon){
+        int srcAgent=agent.get_curr_edge().getDest();
+        int srcPokemon=pokemon.get_edge().getSrc();
+        int destPokemon=pokemon.get_edge().getDest();
+
+        DWGraph_Algo gAlgo=new DWGraph_Algo();
+        gAlgo.init(_ar.getGraph());
+
+        if(gAlgo.shortestPath(srcAgent,srcPokemon)!=null){
+            if(gAlgo.shortestPath(srcPokemon,destPokemon)!=null){
+                if(gAlgo.shortestPath(srcAgent,srcPokemon).size()==1){
+                    return (ArrayList<node_data>)gAlgo.shortestPath(srcAgent,srcPokemon);
+                }
+                else{
+                   return null;
+                }
+            }
+        }
+        return null;
+    }
+
 
 }
